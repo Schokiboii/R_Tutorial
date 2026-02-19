4e93d65a7ccf938edd56550fe3b5f7435167d8e4
Samuel Suess <samisuess03@gmail.com>
2026-02-19 15:55:00 +0100

    Lession 01

---
+++ 01_Intro_to_R/scripts/Intro_to_R.R
@@ -0,0 +1,30 @@
+# Intro to R Lesson
+# Feb. 18th, 2026
+
+#Interacting with R
+
+#Add 3 and 5
+3 + 5
+
+
+# Assign the value of 3 to the object called x
+x <- 3
+
+# Assign the value of 5 to the object called y
+y <- 5
+
+# Print the object y
+y
+
+# Add x and y
+x + y
+
+# Add x and y and assign their sum to the object called number
+number <- x + y
+
+#Exercise 3
+x <- 5
+y <- 10
+number <- x + y
+
+
